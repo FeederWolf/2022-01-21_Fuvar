aea95f96b1ae919ab1667d53f67f49c2f3d3cb35
FeederWolf <Kollarg22@gmail.com>
2022-01-21 10:25:49 +0100

    Add project files.

---
+++ 2022-01-21_Fuvar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022_01_21_Fuvar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<temitrohogsz> taxi = new List<temitrohogsz>();
+            foreach (var s in File.ReadAllLines("fuvar.csv"))
+            {
+                taxi.Add(new temitrohogsz(s));
+            }
+
+        }
+    }
+}
---
+++ 2022-01-21_Fuvar/temitrohogsz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_01_21_Fuvar
+{
+    class temitrohogsz
+    {
+        public int id { get; set; }
+        public DateTime indul { get; set; }
+        public int idotartalom { get; set; }
+        public double tavolsag { get; set; }
+        public double viteldij { get; set; }
+        public double bor { get; set; }
+        public string bankkartya { get; set; }
+
+        public temitrohogsz(string sor) {
+            string[] s = sor.Split(';');
+            id = int.Parse(s[0]);
+            indul = DateTime.Parse(s[1]);
+            tavolsag = double.Parse(s[2]);
+            viteldij = double.Parse(s[3]);
+            bor = double.Parse(s[4]);
+            bankkartya = s[5];
+
+        }
+
+    }
+}
