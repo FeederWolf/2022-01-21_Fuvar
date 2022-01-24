9013d6f2684876f6c5d66560eb1a8c9e8c43ad15
FeederWolf <Kollarg22@gmail.com>
2022-01-24 11:30:27 +0100

    GUI 1.

---
+++ FuvarGOUI/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace FuvarGOUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnKilepes = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Indulas = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Idotartalom = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Tavolsag = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Viteldij = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.Borravalo = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.Forras_mod = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKilepes
+            // 
+            this.btnKilepes.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnKilepes.BackgroundImage")));
+            this.btnKilepes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKilepes.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnKilepes.ForeColor = System.Drawing.Color.Red;
+            this.btnKilepes.Location = new System.Drawing.Point(648, 388);
+            this.btnKilepes.Name = "btnKilepes";
+            this.btnKilepes.Size = new System.Drawing.Size(140, 50);
+            this.btnKilepes.TabIndex = 0;
+            this.btnKilepes.Text = "kilépés";
+            this.btnKilepes.UseVisualStyleBackColor = true;
+            this.btnKilepes.Click += new System.EventHandler(this.btnKilepes_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Snap ITC", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(274, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fuvar Adatai";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 94);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Indulas
+            // 
+            this.Indulas.AutoSize = true;
+            this.Indulas.Location = new System.Drawing.Point(267, 100);
+            this.Indulas.Name = "Indulas";
+            this.Indulas.Size = new System.Drawing.Size(48, 15);
+            this.Indulas.TabIndex = 6;
+            this.Indulas.Text = "Indulás:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(321, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "TaxiID:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(321, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Idotartalom
+            // 
+            this.Idotartalom.AutoSize = true;
+            this.Idotartalom.Location = new System.Drawing.Point(267, 144);
+            this.Idotartalom.Name = "Idotartalom";
+            this.Idotartalom.Size = new System.Drawing.Size(48, 15);
+            this.Idotartalom.TabIndex = 6;
+            this.Idotartalom.Text = "Idotart.:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(321, 182);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Tavolsag
+            // 
+            this.Tavolsag.AutoSize = true;
+            this.Tavolsag.Location = new System.Drawing.Point(260, 190);
+            this.Tavolsag.Name = "Tavolsag";
+            this.Tavolsag.Size = new System.Drawing.Size(55, 15);
+            this.Tavolsag.TabIndex = 6;
+            this.Tavolsag.Text = "Távolság:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(321, 228);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 1;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Viteldij
+            // 
+            this.Viteldij.AutoSize = true;
+            this.Viteldij.Location = new System.Drawing.Point(269, 236);
+            this.Viteldij.Name = "Viteldij";
+            this.Viteldij.Size = new System.Drawing.Size(46, 15);
+            this.Viteldij.TabIndex = 6;
+            this.Viteldij.Text = "Viteldíj:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(321, 275);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 23);
+            this.textBox6.TabIndex = 1;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Borravalo
+            // 
+            this.Borravalo.AutoSize = true;
+            this.Borravalo.Location = new System.Drawing.Point(255, 283);
+            this.Borravalo.Name = "Borravalo";
+            this.Borravalo.Size = new System.Drawing.Size(60, 15);
+            this.Borravalo.TabIndex = 6;
+            this.Borravalo.Text = "Borravaló:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(321, 325);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 23);
+            this.textBox7.TabIndex = 1;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Forras_mod
+            // 
+            this.Forras_mod.AutoSize = true;
+            this.Forras_mod.Location = new System.Drawing.Point(236, 333);
+            this.Forras_mod.Name = "Forras_mod";
+            this.Forras_mod.Size = new System.Drawing.Size(79, 15);
+            this.Forras_mod.TabIndex = 6;
+            this.Forras_mod.Text = "Forrás módja:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Forras_mod);
+            this.Controls.Add(this.Borravalo);
+            this.Controls.Add(this.Viteldij);
+            this.Controls.Add(this.Tavolsag);
+            this.Controls.Add(this.Idotartalom);
+            this.Controls.Add(this.Indulas);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnKilepes);
+            this.Name = "Form1";
+            this.Text = "Esemény kezelő metódus";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKilepes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label Indulas;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Idotartalom;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label Tavolsag;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label Viteldij;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label Borravalo;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label Forras_mod;
+    }
+}
+
---
+++ FuvarGOUI/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuvarGOUI
+{
+    public partial class Form1 : Form
+    {
+        public int lajos = 0;
+        public string valami = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKilepes_Click(object sender, EventArgs e)
+        {
+            if (lajos == 1)
+            {
+                Close();
+            }
+            else if (lajos == 0)
+            {
+                valami = "Az igen";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = valami;
+        }
+    }
+}
